b209fa5cdcf417e4b47402ca1baddf090f006137
nikhil <nikhilchawla9013@gmail.com>
2024-01-29 23:39:08 +0530

    add script commands for build and run

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1" , {
-    id:"heading"
-} , "Hi it is React.");
+
+// normal react element
+// const heading = React.createElement("h1" , {
+//     id:"heading"
+// } , "Hi it is React.");
 
 
-console.log(heading);//it is a react obj.
+// console.log(heading);//it is a react obj.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -21,14 +23,41 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
     </div> */}
     // code for this
     
-    const element = React.createElement(
-        "div" , {id:"parent"},
-        React.createElement("div",{id:"child"}, 
-        [
-            React.createElement("h1",{},"Heading1"),
-            React.createElement("h2",{},"Heading2"),
-        ]
-        ) //if multiple element then array else single element
-        )
-        console.log(element);
-        root.render(element);
+    // const element = React.createElement(
+    //     "div" , {id:"parent"},
+    //     React.createElement("div",{id:"child"}, 
+    //     [
+    //         React.createElement("h1",{},"Heading1"),
+    //         React.createElement("h2",{},"Heading2"),
+    //     ]
+    //     ) //if multiple element then array else single element
+    //     )
+    //     console.log(element);
+    //     root.render(element);
+
+
+    // jsx element 
+
+    // jsx element => babel transpiles to react element
+
+        // const jsxHeading = (
+        //     <h1 className="heading" tabIndex="5">
+        //         It is react JSX heading
+        //     </h1>
+        // )
+        // console.log(jsxHeading);
+        // root.render(jsxHeading);
+
+
+        // functional componeny
+
+
+        const Title = () => <h1>Title of Namaste React</h1>
+
+        const HeadingComponent = () => (
+            <div className="heading">
+                <Title/>
+                <h1>Namaste React</h1>
+            </div>
+        );
+        root.render(<HeadingComponent/>)
